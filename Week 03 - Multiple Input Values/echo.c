d6694c2dc16ff31da445d35da2bd866ced479bc0
Troy DeSpain <despaintroy@gmail.com>
2020-09-16 09:14:36 -0600

    Week 3 sample code

---
+++ Week 03 - Multiple Input Values/echo.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // Declare variables
+    int a, b, c;
+
+    // Get three values from the user
+    printf("Enter three integers: ");
+    scanf_s("%d %d %d", &a, &b, &c);
+
+    // Print the same values back to the user
+    printf("The three values are %d, %d, and %d.", a, b, c);
+
+    getchar();
+    getchar();
+    return 0;
+}
